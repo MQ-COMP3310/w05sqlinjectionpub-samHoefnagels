9ded853bd0fe2ee6414b24a7755d90816bef8f2e
samHoefnagels <samantha@getpacked.com.au>
2025-03-28 10:27:22 +1100

    validating importing untrusting strings

--- app/src/main/java/workshop05code/App.java
+++ app/src/main/java/workshop05code/App.java
@@ -56,9 +56,11 @@ public class App {
             String line;
             int i = 1;
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
-                wordleDatabaseConnection.addValidWord(i, line);
-                i++;
+                if(line.matches("^[a-z]{4}$")){
+                    System.out.println(line);
+                    wordleDatabaseConnection.addValidWord(i, line);
+                    i++;
+                }
             }
 
         } catch (IOException e) {
@@ -75,13 +77,17 @@ public class App {
 
             while (!guess.equals("q")) {
                 System.out.println("You've guessed '" + guess+"'.");
-
-                if (wordleDatabaseConnection.isValidWord(guess)) { 
+                
+                if(guess.matches("^[a-z]{4}$")){
+                    if(wordleDatabaseConnection.isValidWord(guess)){
                     System.out.println("Success! It is in the the list.\n");
+                    }else{
+                    System.out.println("Sorry. This word is NOT in the the list.\n");
+                    }
                 }else{
                     System.out.println("Sorry. This word is NOT in the the list.\n");
                 }
-
+                
                 System.out.print("Enter a 4 letter word for a guess or q to quit: " );
                 guess = scanner.nextLine();
             }
